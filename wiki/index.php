c1955cc691639ca32a08a55d30fecf6dd19c9fa5
Cesar <cesarjesparza@gmail.com>
2015-06-15 11:04:52 -0700

    Style changes

--- wiki/index.php
+++ wiki/index.php
@@ -8,17 +8,20 @@ session_start();
 <main>
 
 	<div id="home">
-		<h2>The IT KnowledgeBase</h2>
-			<p>Knowledge bases are commonly used to complement a help desk. Similar to an FAQ (frequently asked questions), a knowledge base is designed to organize and present your most common customer questions or problems and an explanation of how to solve them.  A knowledge base usually stores troubleshooting information, how-to articles, user manuals, and answers to frequently asked questions.</p>
-			
-			<p>Typically, a search engine is used to locate information in the system, or users may browse through a custom classification scheme.</p>
-			
-			<h3>How Businesses Use The Knowledge Base</h3>
-			
-			<p>Instead of answering the same question over and over, businesses save time and money by directing customers to a online knowledge base before having them submit or open a ticket or call support directly.</p>
-			
-			<p>A knowledge base can also keep customers updated with news, announcements, release information, and product updates.</p>
-	</div>
+        <article>
+            <h2>The IT KnowledgeBase</h2>
+                <p>Knowledge bases are commonly used to complement a help desk. Similar to an FAQ (frequently asked questions), a knowledge base is designed to organize and present your most common customer questions or problems and an explanation of how to solve them.  A knowledge base usually stores troubleshooting information, how-to articles, user manuals, and answers to frequently asked questions.</p>
+
+                <p>Typically, a search engine is used to locate information in the system, or users may browse through a custom classification scheme.</p>
+
+                <h3>How Businesses Use The Knowledge Base</h3>
+
+                <p>Instead of answering the same question over and over, businesses save time and money by directing customers to a online knowledge base before having them submit or open a ticket or call support directly.</p>
+
+                <p>A knowledge base can also keep customers updated with news, announcements, release information, and product updates.</p>
+	   </article>
+    </div><!-- end .home -->
+    
 	
 
 <?php include('includes/footer.php'); ?>
